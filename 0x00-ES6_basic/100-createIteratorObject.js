2a33b6f3a5041d546c85369bfbf7cff42d229cab
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2024-05-27 16:43:24 +0200

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,8 @@
 export default function createIteratorObject(report) {
-  const allEmployees = Object.values(report.allEmployees).flat();
-  return allEmployeesSymbol.iterator;
-}
+    let result = [];
+    for (const value of Object.values(report.allEmployees)) {
+      result = [...result, ...value];
+    }
+
+    return result;
+  }
