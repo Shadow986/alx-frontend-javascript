79b9883176b28a468b6c814bebbd97adffff0101
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2024-05-30 16:14:07 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,21 @@
-// Class EVCar
-/* eslint-disable */
 import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
+
     this._range = range;
   }
 
   cloneCar() {
-    return new Car();
+    const clone = new (Object.getPrototypeOf(this.constructor))(
+      this._brand,
+      this._motor,
+      this._color,
+    );
+
+    clone._range = this._range;
+
+    return clone;
   }
 }
