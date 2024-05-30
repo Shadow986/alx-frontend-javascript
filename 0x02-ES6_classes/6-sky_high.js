18c6206a3e205d24362bb424e621a6523bd2fc66
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2024-05-30 16:20:17 +0200

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,17 @@
-import Building from './5-building.js';
+// Extends class building
+import Building from './5-building';
 
-class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() { return this._sqft; }
 
-    get floors() {
-        return this._floors;
-    }
+  get floors() { return this.floors; }
 
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
-
-export default SkyHighBuilding;
